35c85d9b2bec25455ce7c469c6763fb07081917d
saranya9797 <saranyasasi9725@gmail.com>
2021-02-02 12:56:28 +0530

    adding second commit

---
+++ for/prime.py
@@ -0,0 +1,12 @@
+num=int(input("enter the number"))
+flg=0
+for i in range(2,num):
+    if(num%i==0):
+        flg=1
+        break
+    else:
+        flg=0
+        if(flg==0):
+            print("prime")
+        else:
+            print("not prime")
---
+++ for/range.py
@@ -0,0 +1,6 @@
+num=int(input("enter the input"))
+for i in range(20,50):
+    if(num>=20)&(num<=50):
+        print("true")
+    else:
+        print("false")
